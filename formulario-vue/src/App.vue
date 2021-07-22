8a6371ec38cb1522b7e19e874eeeda701ae38ee6
maicom goncalves <gonm202@gmail.com>
2021-07-22 16:13:14 -0400

    enviar e carregar firebase

--- formulario-vue/src/App.vue
+++ formulario-vue/src/App.vue
@@ -8,15 +8,13 @@
       <h4>POSTO DE SAÚDE INDÍGENA</h4>
     </header>
     <article>
-      <form id="form" v-on:submit="addUser">
+      <form id="form" v-on:submit.prevent="addUser">
         <div>
           <label for="nome">Nome</label>
           <input id="nameId" type="text" v-model="newUsers.nameId" required />
           <label for="sobrenome">Sobrenome</label>
           <input id="lastnameId" type="text" v-model="newUsers.lastnameId" required />
         </div>
-
-        <button type="reset" id="loadButton">Carregar</button>
         <button type="submit" value="AddUser">Enviar</button>
       </form>
       <li v-for="user in users" class="user" :key="user['.key']">
@@ -41,7 +39,7 @@ export default {
           lastnameId: ""}
     };
   },
-  props: {
+ props: {
     newUser: {
       required: false,
       default: () => {
@@ -57,57 +55,20 @@ export default {
         users: db.collection('users'),
       }
   },
-   /*firestore(nameId,lastnameId) {
-     console.log(nameId+'')
-    return {
-      users: db.collection("users").add({ nome:nameId+':id', sobrenome:lastnameId+':id'})
-          .then(() => {
-            console.log("Document successfully written!");
-          })
-          .catch((error) => {
-            console.error("Error writing document: ", error);
-          })
-    };
-  },*/
-
+  
   methods: { 
-    addUser: function () {
-     console.log(this.newUsers.nameId);
-      //this.$firestore.users.add(this.newUser.nameId,this.newUser.lastnameId)
-      this.$firestore().db.collection("users")
-      .add({ nome:this.newUser.nameId+':idwudund', sobrenome:this.newUser.lastnameId+':iddwdwd'})
-      .then(() => {
-            console.log("Document successfully written!");
-          })
-          .catch((error) => {
-            console.error("Error writing document: ", error);
-          });
-      this.newUsers = '';
+    
+     addUser: function () {
+     console.log('testatando teste');
+      db.collection('users').add({ nome:`${this.newUsers.nameId}`
+      , sobrenome:`${this.newUsers.lastnameId}` });
+              
     },
     
     deleteUser: function (user) {
       this.$firestore.users.doc(user[".key"]).delete();
     }
     },
-   
-  
-  /*methods: {
-    this.newUser.nameId = "";
-     this.newUser.lastnameId = "";
-     
-    addUsers: function () {
-      this.$firestore.users.add({
-        name: this.newUser.nameId,
-        lastname: this.newUser.lastnameId,
-        this.newUser.nameId = "";
-          this.newUser.lastnameId = "";
-      });
-      this.newUsers = "";
-    },
-    deleteUser: function (paciente) {
-      this.$firestore.users.doc(paciente[".key"]).delete();
-    },
-  },*/
 };
 </script>
 
